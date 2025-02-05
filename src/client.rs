5f341146393a7d4efd9f4dd5fda5591f312474c5
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-02-05 21:59:23 +0600

    Added logging

--- src/client.rs
+++ src/client.rs
@@ -10,11 +10,14 @@ pub struct Client {
 
 impl Client {
     pub async fn connect(addr: &str) -> Result<Self, std::io::Error> {
+        println!("[CLIENT] Connecting to {}", addr);
         let stream = TcpStream::connect(&addr).await?;
+        println!("[CLIENT] Connected successfully");
         Ok(Client { stream })
     }
 
     pub async fn send_command(&mut self, cmd: &str) -> Result<(), std::io::Error> {
+        println!("[CLIENT] Sending command: {}", cmd.trim());
         self.stream.write_all(cmd.as_bytes()).await?;
         self.stream.write_all(b"\n").await?;
         self.stream.flush().await
@@ -24,6 +27,7 @@ impl Client {
         let mut reader = BufReader::new(&mut self.stream);
         let mut response = String::new();
         reader.read_line(&mut response).await?;
+        println!("[CLIENT] Received response: {}", response.trim());
         Ok(response)
     }
 }
--- src/network.rs
+++ src/network.rs
@@ -14,6 +14,8 @@ use crate::{
 };
 
 pub async fn start_server() -> Result<(), BrokerError> {
+    println!("[BROKER] Starting broker on 127.0.0.1:8080");
+
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     let broker = Broker::new();
 
@@ -27,6 +29,8 @@ pub async fn start_server() -> Result<(), BrokerError> {
 }
 
 pub async fn handle_client(socket: TcpStream, broker: Broker) {
+    println!("[CLIENT] New connection established");
+
     let (read, write) = socket.into_split();
     let mut reader = BufReader::new(read);
     let writer = Arc::new(Mutex::new(BufWriter::new(write)));
@@ -36,13 +40,19 @@ pub async fn handle_client(socket: TcpStream, broker: Broker) {
         line.clear();
 
         match reader.read_line(&mut line).await {
-            Ok(0) => break,
+            Ok(0) => {
+                println!("[CLIENT] Connection closed");
+                break;
+            }
             Ok(_) => match parse_command(&line) {
-                Ok(cmd) => process_command(cmd, &broker, &writer).await,
-                Err(e) => eprintln!("Error: {}", e),
+                Ok(cmd) => {
+                    println!("[CLIENT] Received command: {}", line.trim());
+                    process_command(cmd, &broker, &writer).await
+                }
+                Err(e) => eprintln!("[BROKER] Error: {}", e),
             },
             Err(e) => {
-                eprintln!("Error reading: {}", e);
+                eprintln!("[BROKER] Error reading: {}", e);
                 break;
             }
         }
@@ -56,42 +66,63 @@ pub async fn process_command(
 ) {
     match cmd {
         BrokerCommand::CreateTopic(topic) => {
+            println!("[BROKER] Creating topic: {}", topic);
             if let Err(e) = broker.create_topic(&topic).await {
+                println!("[BROKER] Error creating topic: {}", e);
                 let mut writer = writer.lock().await;
-                let _ = writer.write_all(format!("Error: {}\n", e).as_bytes()).await;
+                let _ = writer
+                    .write_all(format!("[BROKER] Error: {}\n", e).as_bytes())
+                    .await;
+            } else {
+                println!("[BROKER] Topic created: {}", topic);
             }
         }
-        BrokerCommand::Subscribe(topic) => match broker.subscribe(&topic).await {
-            Ok(mut rx) => {
-                let writer = writer.clone();
-                tokio::spawn(async move {
-                    while let Some(msg) = rx.recv().await {
-                        let data = format!(
-                            "TOPIC {} DATA {}\n",
-                            msg.topic,
-                            BASE64_STANDARD.encode(&msg.data)
-                        );
-                        let mut writer = writer.lock().await;
-                        let _ = writer.write_all(data.as_bytes()).await;
-                    }
-                });
+        BrokerCommand::Subscribe(topic) => {
+            println!("[BROKER] New subscription to: {}", topic);
+            match broker.subscribe(&topic).await {
+                Ok(mut rx) => {
+                    println!("[BROKER] Subscription successful: {}", topic);
+                    let writer = writer.clone();
+                    tokio::spawn(async move {
+                        while let Some(msg) = rx.recv().await {
+                            println!("[BROKER] Sending message to subscriber: {:?}", msg);
+                            let data = format!(
+                                "TOPIC {} DATA {}\n",
+                                msg.topic,
+                                BASE64_STANDARD.encode(&msg.data)
+                            );
+                            let mut writer = writer.lock().await;
+                            let _ = writer.write_all(data.as_bytes()).await;
+                        }
+                    });
+                }
+                Err(e) => {
+                    println!("[BROKER] Subscription error: {}", e);
+                    let mut writer = writer.lock().await;
+                    let _ = writer
+                        .write_all(format!("[BROKER] Error: {}\n", e).as_bytes())
+                        .await;
+                }
             }
-            Err(e) => {
-                let mut writer = writer.lock().await;
-                let _ = writer.write_all(format!("Error: {}\n", e).as_bytes()).await;
-            }
-        },
+        }
         BrokerCommand::Unsubscribe(topic) => {
+            println!("[BROKER] Unsubscribing from: {}", topic);
             if let Err(e) = broker.unsubscribe(&topic).await {
                 let mut writer = writer.lock().await;
-                let _ = writer.write_all(format!("Error: {}\n", e).as_bytes()).await;
+                let _ = writer
+                    .write_all(format!("[BROKER] Error: {}\n", e).as_bytes())
+                    .await;
             }
         }
         BrokerCommand::Publish(msg) => {
+            println!("[BROKER] Publishing message to: {}", msg.topic);
             if let Err(e) = broker.publish(msg).await {
                 let mut writer = writer.lock().await;
-                let _ = writer.write_all(format!("Error: {}\n", e).as_bytes()).await;
+                let _ = writer
+                    .write_all(format!("[BROKER] Error: {}\n", e).as_bytes())
+                    .await;
             }
+            println!("[BROKER] Message published successfully");
         }
     }
 
